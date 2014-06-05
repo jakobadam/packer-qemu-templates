83a71f0867e3a922df1a147c0e7c032974bfd096
Jakob Aarøe Dam <jakob.a.dam@gmail.com>
2014-06-05 14:34:19 +0200

    Using glob to include floppy files

--- windows/templates/windows-2012R2-standard-amd64/configure.bat
+++ windows/templates/windows-2012R2-standard-amd64/configure.bat
@@ -1,5 +1,6 @@
+cmd.exe /c a:\disable-hibernate.bat
 cmd.exe /c a:\enable-ping.bat
 cmd.exe /c a:\firewall-enable-rdp.bat
 cmd.exe /c a:\firewall-open-ping.bat
+cmd.exe /c a:\firewall-open-smb.bat
 cmd.exe /c a:\install-cygwin-sshd.bat
-cmd.exe /c a:\disable-hibernate.bat
