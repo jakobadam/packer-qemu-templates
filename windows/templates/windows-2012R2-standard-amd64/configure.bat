7be27e169df94233bc0babace47172a01a57c83d
Jakob Aarøe Dam <jakob.a.dam@gmail.com>
2014-06-05 14:07:37 +0200

    Get rid of some XML -- moved commands to bat script

---
+++ windows/templates/windows-2012R2-standard-amd64/configure.bat
@@ -0,0 +1,5 @@
+cmd.exe /c a:\enable-ping.bat
+cmd.exe /c a:\firewall-enable-rdp.bat
+cmd.exe /c a:\firewall-open-ping.bat
+cmd.exe /c a:\install-cygwin-sshd.bat
+cmd.exe /c a:\disable-hibernate.bat
