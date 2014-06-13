8376530b5256845bcc35816652a29d26db7662ca
Jakob Aarøe Dam <jakob.a.dam@gmail.com>
2014-06-13 16:08:10 +0200

    Enable RDP in template

--- windows/templates/windows-2012R2-standard-amd64/configure.bat
+++ windows/templates/windows-2012R2-standard-amd64/configure.bat
@@ -1,7 +1,8 @@
 cmd.exe /c a:\disable-hibernate.bat
 cmd.exe /c a:\enable-ping.bat
+cmd.exe /c a:\enable-rdp.bat
+cmd.exe /c a:\execution-policy-unrestricted.bat
 cmd.exe /c a:\firewall-enable-rdp.bat
 cmd.exe /c a:\firewall-open-ping.bat
 cmd.exe /c a:\firewall-open-smb.bat
 cmd.exe /c a:\install-cygwin-sshd.bat
-cmd.exe /c a:\execution-policy-unrestricted.bat
