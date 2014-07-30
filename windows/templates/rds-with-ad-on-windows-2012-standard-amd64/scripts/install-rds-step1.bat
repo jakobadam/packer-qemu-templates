8b5fbee924c56c93c96a63ecca7a78f87690d3f7
Jakob Aarøe Dam <jakob.a.dam@gmail.com>
2014-07-30 10:46:41 +0200

    Comments

--- windows/templates/rds-with-ad-on-windows-2012-standard-amd64/scripts/install-rds-step1.bat
+++ windows/templates/rds-with-ad-on-windows-2012-standard-amd64/scripts/install-rds-step1.bat
@@ -1,6 +1,6 @@
-REM Execute step1, schedule step2 and reboot
+:: Installing RDS Step 1
+powershell.exe -File %~dp0install-rds-step1.ps1
 
-powershell.exe -File %~dp0rds-install-step1.ps1
-schtasks /create /tn "RDS Install Step 2" /tr %~dp0rds-install-step2.bat /sc onlogon
-shutdown /r /t 5 /f /c "RDS Install Shutdown Step 1 Finished"
-
+:: Scheduling Install RDS Step 2
+schtasks /create /tn "Install RDS Step 2" /tr %~dp0install-rds-step2.bat /sc onlogon
+shutdown /r /t 5 /f /c "Install RDS Step 1 Finished"
