63535dbd115c3a3e78655120695b2955c2b8ba1a
Jakob Aarøe Dam <jakob.a.dam@gmail.com>
2014-07-30 10:45:29 +0200

    Let the scripts be run from any location

--- windows/templates/rds-with-ad-on-windows-2012-standard-amd64/configure.bat
+++ windows/templates/rds-with-ad-on-windows-2012-standard-amd64/configure.bat
@@ -1,8 +1,16 @@
-cmd.exe /c a:\disable-hibernate.bat
-cmd.exe /c a:\enable-rdp.bat
-cmd.exe /c a:\execution-policy-unrestricted.bat
-cmd.exe /c a:\firewall-disable.bat
-cmd.exe /c a:\install-rds.bat
+:: %~dp0 gives the dirname of the script
+:: we use this so it's easy to test the script from both
+:: packer and samba etc.
 
-REM Cygwin is installed via scheduled task setup by install-rds
+cmd.exe /c %~dp0disable-hibernate.bat
+cmd.exe /c %~dp0enable-rdp.bat
+cmd.exe /c %~dp0execution-policy-unrestricted.bat
+cmd.exe /c %~dp0firewall-disable.bat
 
+:: Install Remote Desktop Services
+:: Restarts the machine several times
+cmd.exe /c %~dp0install-rds.bat
+
+:: NOTE: Cygwin is installed via scheduled task setup by install-rds
+:: This is done as the final step since packer shuts down the machine
+:: when ssh is ready
