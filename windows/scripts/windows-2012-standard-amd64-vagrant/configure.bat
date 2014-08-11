288f133b4d52cba6ee71d70a7859318654519f7a
Jakob Aarøe Dam <jakob.a.dam@gmail.com>
2014-08-11 16:01:17 +0200

    Unlimited password expiration for windows

--- windows/scripts/windows-2012-standard-amd64-vagrant/configure.bat
+++ windows/scripts/windows-2012-standard-amd64-vagrant/configure.bat
@@ -1,8 +1,5 @@
 :: %~dp0 gives the dirname of the script
 
-cmd /c %~dp0disable-hibernate.bat
-cmd /c %~dp0enable-rdp.bat
-cmd /c %~dp0execution-policy-unrestricted.bat
-cmd /c %~dp0firewall-disable.bat
-cmd /c %~dp0install-cygwin-sshd.bat
+cmd /c %~dp0common.bat
 cmd /c %~dp0vagrant-ssh.bat
+
