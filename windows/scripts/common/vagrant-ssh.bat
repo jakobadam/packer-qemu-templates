eb1743bc481d0165b9a44125b5841266b7b8d246
Jakob Aarøe Dam <jakob.a.dam@gmail.com>
2014-09-01 12:07:58 +0200

    Move (most) provisioning to provisioners section

--- windows/scripts/common/vagrant-ssh.bat
+++ windows/scripts/common/vagrant-ssh.bat
@@ -4,11 +4,7 @@ if not exist %USERDIR%\vagrant\.ssh (
   mkdir %USERDIR%\vagrant\.ssh
 )
 
-if exist %~dp0vagrant.pub (
-  copy %~dp0vagrant.pub %USERDIR%\vagrant\.ssh\authorized_keys
-) else (
-    C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -Command "(New-Object System.Net.WebClient).DownloadFile('https://raw.github.com/mitchellh/vagrant/master/keys/vagrant.pub', 'C:\Users\vagrant\.ssh\authorized_keys')" <NUL
-)
+C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -Command "(New-Object System.Net.WebClient).DownloadFile('https://raw.github.com/mitchellh/vagrant/master/keys/vagrant.pub', 'C:\Users\vagrant\.ssh\authorized_keys')" <NUL
 
 cd %USERDIR%\vagrant
 cmd /c %SystemDrive%\cygwin\bin\bash -c 'PATH=/usr/local/bin:/usr/bin:/bin:/usr/X11R6/bin chown -R vagrant .ssh'
