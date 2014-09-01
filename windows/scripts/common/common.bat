ac9b1ea11c7f5ac8a61e8db388517c791978675c
Jakob Aarøe Dam <jakob.a.dam@gmail.com>
2014-09-01 10:32:16 +0200

    Move (most) provisioning to packer provisioners section

--- windows/scripts/common/common.bat
+++ windows/scripts/common/common.bat
@@ -1,6 +1,2 @@
-cmd /c %~dp0disable-hibernate.bat
-cmd /c %~dp0enable-rdp.bat
-cmd /c %~dp0execution-policy-unrestricted.bat
 cmd /c %~dp0firewall-disable.bat
-cmd /c %~dp0unlimited-password-expiration.bat
 cmd /c %~dp0install-cygwin-sshd.bat
