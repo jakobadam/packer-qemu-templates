d6cd5b778c85bbbf30af7574a97fa8e401adcb7f
Jakob Aarøe Dam <jakob.a.dam@gmail.com>
2014-09-08 10:06:11 +0200

    Pre-install nfs for vagrant template

--- ubuntu/scripts/vagrant.sh
+++ ubuntu/scripts/vagrant.sh
@@ -11,3 +11,6 @@ mkdir -pm 700 /home/vagrant/.ssh
 wget --no-check-certificate https://raw.github.com/mitchellh/vagrant/master/keys/vagrant.pub -O /home/vagrant/.ssh/authorized_keys
 chmod 0600 /home/vagrant/.ssh/authorized_keys
 chown -R vagrant:vagrant /home/vagrant/.ssh
+
+# NFS used for file syncing
+apt-get --yes install nfs-common
