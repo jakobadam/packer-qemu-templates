95a523f771315a27fcdce39dca0c50e21a6c0bd5
Jakob Aarøe Dam <jakob.a.dam@gmail.com>
2016-10-05 19:47:46 +0200

    Rename the network interface so Ubuntu 16.04 gets an IP on first boot.

--- ubuntu/scripts/cleanup.sh
+++ ubuntu/scripts/cleanup.sh
@@ -19,16 +19,16 @@ fi
 
 UBUNTU_VERSION=$(lsb_release -sr)
 if [[ ${UBUNTU_VERSION} == 16.04 ]] || [[ ${UBUNTU_VERSION} == 16.10 ]]; then
-    # from https://github.com/cbednarski/packer-ubuntu/blob/master/scripts-1604/vm_cleanup.sh#L9-L15
-    # When booting with Vagrant / VMware the PCI slot is changed from 33 to 32.
-    # Instead of eth0 the interface is now called ens33 to mach the PCI slot,
-    # so we need to change the networking scripts to enable the correct
-    # interface.
+    # Modified version of
+    # https://github.com/cbednarski/packer-ubuntu/blob/master/scripts-1604/vm_cleanup.sh#L9-L15
+    # Instead of eth0 the interface is now called ens5 to mach the PCI
+    # slot, so we need to change the networking scripts to enable the
+    # correct interface.
     #
     # NOTE: After the machine is rebooted Packer will not be able to reconnect
     # (Vagrant will be able to) so make sure this is done in your final
     # provisioner.
-    sed -i "s/ens33/ens32/g" /etc/network/interfaces
+    sed -i "s/ens3/ens5/g" /etc/network/interfaces
 fi
 
 # Add delay to prevent "vagrant reload" from failing
@@ -60,45 +60,48 @@ echo "==> Clearing last login information"
 >/var/log/wtmp
 >/var/log/btmp
 
-# Whiteout root
-count=$(df --sync -kP / | tail -n1  | awk -F ' ' '{print $4}')
-let count--
-dd if=/dev/zero of=/tmp/whitespace bs=1024 count=$count
-rm /tmp/whitespace
+# NOTE: Shrinking is not part of the build process
+# so this will only grow the image...
 
-# Whiteout /boot
-count=$(df --sync -kP /boot | tail -n1 | awk -F ' ' '{print $4}')
-let count--
-dd if=/dev/zero of=/boot/whitespace bs=1024 count=$count
-rm /boot/whitespace
+# # Whiteout root
+# count=$(df --sync -kP / | tail -n1  | awk -F ' ' '{print $4}')
+# let count--
+# dd if=/dev/zero of=/tmp/whitespace bs=1024 count=$count
+# rm /tmp/whitespace
 
-echo '==> Clear out swap and disable until reboot'
-set +e
-swapuuid=$(/sbin/blkid -o value -l -s UUID -t TYPE=swap)
-case "$?" in
-    2|0) ;;
-    *) exit 1 ;;
-esac
-set -e
-if [ "x${swapuuid}" != "x" ]; then
-    # Whiteout the swap partition to reduce box size
-    # Swap is disabled till reboot
-    swappart=$(readlink -f /dev/disk/by-uuid/$swapuuid)
-    /sbin/swapoff "${swappart}"
-    dd if=/dev/zero of="${swappart}" bs=1M || echo "dd exit code $? is suppressed"
-    /sbin/mkswap -U "${swapuuid}" "${swappart}"
-fi
+# # Whiteout /boot
+# count=$(df --sync -kP /boot | tail -n1 | awk -F ' ' '{print $4}')
+# let count--
+# dd if=/dev/zero of=/boot/whitespace bs=1024 count=$count
+# rm /boot/whitespace
 
-# Zero out the free space to save space in the final image
-dd if=/dev/zero of=/EMPTY bs=1M  || echo "dd exit code $? is suppressed"
-rm -f /EMPTY
+# echo '==> Clear out swap and disable until reboot'
+# set +e
+# swapuuid=$(/sbin/blkid -o value -l -s UUID -t TYPE=swap)
+# case "$?" in
+#     2|0) ;;
+#     *) exit 1 ;;
+# esac
+# set -e
+# if [ "x${swapuuid}" != "x" ]; then
+#     # Whiteout the swap partition to reduce box size
+#     # Swap is disabled till reboot
+#     swappart=$(readlink -f /dev/disk/by-uuid/$swapuuid)
+#     /sbin/swapoff "${swappart}"
+#     dd if=/dev/zero of="${swappart}" bs=1M || echo "dd exit code $? is suppressed"
+#     /sbin/mkswap -U "${swapuuid}" "${swappart}"
+# fi
 
-# Make sure we wait until all the data is written to disk, otherwise
-# Packer might quite too early before the large files are deleted
-sync
+# # Zero out the free space to save space in the final image
+# dd if=/dev/zero of=/EMPTY bs=1M  || echo "dd exit code $? is suppressed"
+# rm -f /EMPTY
 
-echo "==> Disk usage before cleanup"
-echo ${DISK_USAGE_BEFORE_CLEANUP}
+# # Make sure we wait until all the data is written to disk, otherwise
+# # Packer might quite too early before the large files are deleted
+# sync
 
-echo "==> Disk usage after cleanup"
-df -h
+# echo "==> Disk usage before cleanup"
+# echo ${DISK_USAGE_BEFORE_CLEANUP}
+
+# echo "==> Disk usage after cleanup"
+# df -h
